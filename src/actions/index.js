d6fddcb7c84811632c1b1e1629b8f1b50bfd6402
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2022-01-23 19:35:06 +0900

    learn redux

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+
+const increment = () => {
+  return (
+    {
+      type: "INCREMENT"
+    }
+  )
+}
+
+const decrement = () => {
+  return (
+    {
+      type: "DECREMENT"
+    }
+  )
+}
